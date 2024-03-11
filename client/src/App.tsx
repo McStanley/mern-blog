82b7c47935372c0c1fa1dc478387956af15faf68
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2024-03-11 13:40:51 +0100

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>MERN Blog</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import logger from 'morgan';
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+export default app;
